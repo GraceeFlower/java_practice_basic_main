1a4f46ea097f2c2175b848a558759a14e66decf9
GraceeFlower <1491644103@qq.com>
2020-02-05 21:53:29 +0800

    [WM] feat: print order details

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -1,5 +1,6 @@
 package com.thoughtworks;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
@@ -12,6 +13,19 @@ public class App {
     System.out.println(summary);
   }
 
+  class item {
+    String id;
+    String name;
+    int count;
+    double price;
+
+    public item(String id, String name, int count, double price) {
+      this.id = id;
+      this.name = name;
+      this.count = count;
+      this.price = price;
+    }
+  }
   /**
    * 接收用户选择的菜品和数量，返回计算后的汇总信息
    *
@@ -19,8 +33,33 @@ public class App {
    */
   public static String bestCharge(String selectedItems) {
     // 此处补全代码
-    return selectedItems;
+    String[] items = selectedItems.split(",");
+    String[] ids = getItemIds();
+    String[] itemNames = new String[items.length];
+    String[] itemCounts = new String[items.length];
+    double[] itemPrices = new double[items.length];
+
+    for (int i = 0, j = 0; i < items.length; i++) {
+      String itemId = items[i].split(" x ")[0];
+      String itemCount = items[i].split(" x ")[1];
+      itemCounts[0] = itemCount;
+      for (int k = 0; k < ids.length; k++) {
+        if (itemId.equals(ids[k])) {
+          itemNames[j] = getItemNames()[k];
+          itemPrices[j] = getItemPrices()[k];
+          j++;
+          break;
+        }
+      }
+    }
+    return Arrays.toString(itemNames);
   }
+
+  public static String getItemId() {
+    String itemId = "0";
+    return itemId;
+  }
+
 
   /**
    * 获取每个菜品依次的编号
@@ -50,3 +89,49 @@ public class App {
     return new String[]{"ITEM0001", "ITEM0022"};
   }
 }
+
+
+//public class Item {
+//  String id;
+//  String name;
+//  Double prices;
+//
+//  public Item(String id, String name, Double prices) {
+//    this.id = id;
+//    this.name = name;
+//    this.prices = prices;
+//  }
+//
+//  @Override
+//  public String toString() {
+//    return "Item{" +
+//        "id='" + id + '\'' +
+//        ", name='" + name + '\'' +
+//        ", prices=" + prices +
+//        '}';
+//  }
+//
+//  public String getId() {
+//    return id;
+//  }
+//
+//  public void setId(String id) {
+//    this.id = id;
+//  }
+//
+//  public String getName() {
+//    return name;
+//  }
+//
+//  public void setName(String name) {
+//    this.name = name;
+//  }
+//
+//  public Double getPrices() {
+//    return prices;
+//  }
+//
+//  public void setPrices(Double prices) {
+//    this.prices = prices;
+//  }
+//}
