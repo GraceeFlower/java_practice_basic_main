3c3d9410a58c461fe329a57ccd957e40b65e9b96
GraceeFlower <1491644103@qq.com>
2020-02-06 01:04:20 +0800

    [WM] feat: add the final price after discount to the receipt

--- src/main/java/com/thoughtworks/App.java
+++ src/main/java/com/thoughtworks/App.java
@@ -17,9 +17,9 @@ public class App {
     String id;
     String name;
     String count;
-    Double price;
+    double price;
 
-    public Item(String id, String name, String count, Double price) {
+    public Item(String id, String name, String count, double price) {
       this.id = id;
       this.name = name;
       this.count = count;
@@ -32,7 +32,6 @@ public class App {
    * @param selectedItems 选择的菜品信息
    */
   public static String bestCharge(String selectedItems) {
-    // 此处补全代码
     ArrayList<Item> menu = getMenu(selectedItems);
     int[] subtotals = getSubtotal(menu);
     String receipt = printReceipt(menu, subtotals);
@@ -40,7 +39,7 @@ public class App {
     return receipt;
   }
 
-  public static ArrayList getMenu(String selectedItems) {
+  public static ArrayList<Item> getMenu(String selectedItems) {
     ArrayList<Item> menu = new ArrayList<Item>();
     String[] items = selectedItems.split(",");
     for (int i = 0; i < items.length; i++) {
@@ -62,7 +61,7 @@ public class App {
 
     for (int i = 0; i < subtotals.length; i++) {
       String countStr = menu.get(i).count;
-      Double count = Double.valueOf(countStr);
+      double count = Double.valueOf(countStr);
       int subtotal = (int) (count *  menu.get(i).price);
       subtotals[i] = subtotal;
     }
@@ -71,20 +70,23 @@ public class App {
 
   public static String printReceipt(ArrayList<Item> menu, int[] subtotals) {
     String receipt = "============= 订餐明细 =============\n";
+    String discountMsg = calculatePrice(menu, subtotals);
 
     for (int i = 0; i < menu.size(); i++) {
       receipt = receipt + menu.get(i).name + " x "
           + menu.get(i).count + " = " + subtotals[i] + "元\n";
     }
-    receipt += "===================================";
+    receipt += discountMsg + "===================================";
     return receipt;
   }
 
   public static String calculatePrice(ArrayList<Item> menu, int[] subtotals) {
     int totalPrice = 0;
+
     for (int i: subtotals) {
       totalPrice += i;
     }
+
     int fullOffPrice = calculateMoneyOff(totalPrice);
     ArrayList<Integer> halfOffPrice = calculateRateDiscount(menu, totalPrice);
     int realPrice = totalPrice;
@@ -121,6 +123,7 @@ public class App {
   public static ArrayList<Integer> calculateRateDiscount(ArrayList<Item> menu, int realMoney) {
     ArrayList<Integer> discountInfo = new ArrayList<>();
     int discountPrice = realMoney;
+
     for (int i = 0; i < menu.size(); i++) {
       if (menu.get(i).id.equals(getHalfPriceIds()[0])) {
         discountInfo.add(0);
